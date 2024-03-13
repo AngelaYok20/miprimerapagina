a55db6061e686b0a183d5680165f04c7b399c140
AngelaYok20 <163216241+AngelaYok20@users.noreply.github.com>
2024-03-12 19:40:28 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert('Hola a todos grupo 21' );//
+let nombre = 'Angela';
+let segundonombre = 'Iriana';
+console.log(nombre);
+console.log(segundonombre);
+ if()
+ if (condition) {
+    
+ } else {
+    
+ }
